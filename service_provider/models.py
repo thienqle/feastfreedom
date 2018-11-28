5eaea6b6050798160af66653c45c86b5980e2340
toshetam <agautam@stevens.edu>
2018-11-28 11:22:06 -0500

    Update models.py

--- service_provider/models.py
+++ service_provider/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Provider(models.Model):
-    name = models.CharField(max_length=200)
+class RegularUser(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
+    question1 = models.CharField(max_length=200)
+    answer1 = models.CharField(max_length=200)
+    question2 = models.CharField(max_length=200)
+    answer2 = models.CharField(max_length=200)
