427b3e374eec02df3f485f64b966335f42fb9246
toshetam <agautam@stevens.edu>
2018-11-28 11:15:01 -0500

    provider details

--- service_provider/models.py
+++ service_provider/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class RegularUser(models.Model):
-    firstname = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
+class Provider(models.Model):
+    name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
-    question1 = models.CharField(max_length=200)
-    answer1 = models.CharField(max_length=200)
-    question2 = models.CharField(max_length=200)
-    answer2 = models.CharField(max_length=200)
