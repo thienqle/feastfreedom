ca3ea1cc3e9cdf7bc8280be75e83e1cccc5400ef
toshetam <agautam@stevens.edu>
2018-11-28 12:03:32 -0500

    Update models.py

--- service_provider/models.py
+++ service_provider/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Provider(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+class Kitchen(models.Model):
+    provider_id = models.ForeignKey(Provider, null=False, related_name="provider")
+    kit_days = models.CharField(max_length=200)
+    start_time = models.CharField(max_length=200)
+    end_time = models.CharField(max_length=200)
+    kit_image = models.ImageField(upload_to=os.path.join('media', str(instance.id), filename), blank=True, null=True)
+class Menu(models.Model):
+    kit_id = models.ForeignKey(Provider, null=False, related_name="kitchen")
+    item_name=models.CharField(max_length=200)
