2496d430d2d667d18bf132c2dd91b212391e3cde
thienqle <thienqle@iastate.edu>
2018-11-28 16:57:37 -0500

    Add index template

--- regular_user/views.py
+++ regular_user/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from service_provider.models import Kitchen,Menu
+from service_provider.models import Kitchen,Menu,Provider
 
 # Create your views here.
 def list_all_kitchen(request):
     kitchens = Kitchen.objects.all()
+    #providers = Kitchen.provider.all()
+    #return render(request,'index.html',{'kitchens':kitchens,'providers':providers})
     return render(request,'index.html',{'kitchens':kitchens})
 
 #def login(request)
