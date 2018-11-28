8c2916262fcb3763da78a2cbe81c3ffc3f3c6272
thienqle <thienqle@iastate.edu>
2018-11-28 13:01:58 -0500

    complete models

--- service_provider/admin.py
+++ service_provider/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Kitchen,Provider,Menu
 
 # Register your models here.
+admin.site.register(Kitchen)
+admin.site.register(Provider)
+admin.site.register(Menu)
--- service_provider/models.py
+++ service_provider/models.py
@@ -12,5 +12,7 @@ class Kitchen(models.Model):
     end_time = models.CharField(max_length=200)
     kit_image = models.ImageField(upload_to="service_provider", blank=True, null=True)
 class Menu(models.Model):
-    kit_id = models.ForeignKey(Provider, null=False, related_name="kitchen",on_delete = models.CASCADE)
+    kit_id = models.ForeignKey(Kitchen, null=False, related_name="kitchen",on_delete = models.CASCADE)
+    veg = models.BooleanField(default = False)
+    price = models.IntegerField(default=0)
     item_name=models.CharField(max_length=200)
