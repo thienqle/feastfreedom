906702a2b376d17f6c288d66ee3406c430f37f55
thienqle <thienqle@iastate.edu>
2018-11-28 12:04:58 -0500

    adding regular user model

--- regular_user/models.py
+++ regular_user/models.py
@@ -2,4 +2,12 @@ from django.db import models
 import os
 
 # Create your models here.
-
+class RegularUser(models.Model):
+    fname = models.TextField(blank=True, null=True)
+    lname = models.TextField(blank=True, null=True)
+    email = models.TextField(blank=True, null=True)
+    password = models.TextField(blank=True, null=True)
+    ques1 = models.TextField(blank=True, null=True)
+    ans1 = models.TextField(blank=True, null=True)
+    ques2 = models.TextField(blank=True, null=True)
+    ans2 = models.TextField(blank=True, null=True)
